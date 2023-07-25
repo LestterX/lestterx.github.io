6f8ef192d8b64126f2e9c42b5d66187086f9f85e
LestterX <lgs.alves@hotmail.com>
2023-07-24 21:24:29 -0300

    feat: nav mobi hidden and visible

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,27 @@ let navBar = document.querySelector('.nav-bar')
 
 const display = ['block', 'none']
 let dCount = 0
-bntMobile.addEventListener('click', () => {
+function changeDisplay(){
   navBar.style.display = display[dCount];
   dCount ++;
   if (dCount > 1) dCount = 0;
-})
+}
+
+bntMobile.addEventListener('click', changeDisplay)
+
+document.addEventListener('click', e => {
+  let target = e.target.classList.value
+  console.log(target);
+  console.log(e.target);
+  if(
+    (target !== 'social' && dCount === 1) &&
+    (target !== 'nav-bar' && dCount === 1) &&
+    (target !== 'nav-bar-time' && dCount === 1) &&
+    (target !== 'nav-bar-li' && dCount === 1) &&
+    (target !== 'top-bar' && dCount === 1) &&
+    (target !== 'line1' && dCount === 1) &&
+    (target !== 'line2' && dCount === 1) &&
+    (target !== 'line3' && dCount === 1) &&
+  (target !== 'mobile_bnt' && dCount) === 1
+    ) changeDisplay();
+});
